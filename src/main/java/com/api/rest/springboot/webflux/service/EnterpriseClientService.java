a620c28614de8540dd8d40f4c5dada46534d90a4
Erikson Felipe Napan Villagaray <enapanvi@emeal.nttdata.com>
2022-07-10 21:32:33 -0500

    Clean Repo

--- src/main/java/com/api/rest/springboot/webflux/service/EnterpriseClientService.java
+++
@@ -1,8 +0,0 @@
-package com.api.rest.springboot.webflux.service;
-
-import com.api.rest.springboot.webflux.model.EnterpriseClient;
-import com.api.rest.springboot.webflux.util.Crud;
-
-public interface EnterpriseClientService extends Crud<EnterpriseClient, String>{
-
-}
--- src/main/java/com/api/rest/springboot/webflux/service/PersonalClientService.java
+++
@@ -1,8 +0,0 @@
-package com.api.rest.springboot.webflux.service;
-
-import com.api.rest.springboot.webflux.model.PersonalClient;
-import com.api.rest.springboot.webflux.util.Crud;
-
-public interface PersonalClientService extends Crud<PersonalClient, String>{
-  
-}
